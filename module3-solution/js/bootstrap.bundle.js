aba4b7d6c2e3792cff7c86aeddf3c80aa6b8a7ce
marta4494 <marta.costantini4@gmail.com>
2021-11-05 13:56:34 +0100

    pre-pull-request

--- module3-solution/js/bootstrap.bundle.js
+++ module3-solution/js/bootstrap.bundle.js
@@ -15,7 +15,7 @@
     for (var i = 0; i < props.length; i++) {
       var descriptor = props[i];
       descriptor.enumerable = descriptor.enumerable || false;
-      descriptor.configurable = true;
+      descriptor.configurable = false;
       if ("value" in descriptor) descriptor.writable = true;
       Object.defineProperty(target, descriptor.key, descriptor);
     }
